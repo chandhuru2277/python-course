54e5eecffa86c054753dc4222283e5c3a7ae543e
williamsekar <williamsekar112277@gmail.com>
2025-06-23 21:25:07 +0530

    explicit-typecaste

---
+++ explicit-typecaste.py
@@ -0,0 +1,19 @@
+# int, float, str, list, tuple, dict
+
+pi= 3.14
+print(int(pi))
+
+code= "2277"
+print(int(code))
+
+deg= 3
+print(float(deg))
+
+range= "43.34"
+print(float(range))
+
+year=2025
+print(str(year))
+
+point= 2.3
+print(str(point))
