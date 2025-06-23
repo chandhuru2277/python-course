5a4bf7b85bf9dcfeb6b7b2bd60d0fdcd186a114b
williamsekar <williamsekar112277@gmail.com>
2025-06-23 21:01:53 +0530

    print

---
+++ print.py
@@ -0,0 +1,15 @@
+# normal way
+name = "william"
+print("My name is", name)
+
+# multiple arguments
+salary =1
+joined_year= 2025
+print("salary: ",salary, "joined year: " , joined_year)
+
+# separator
+print("cat","dog", "rat", sep='-')
+
+# end parameter
+print("This year is 2025", end="\t")
+print("It so amazing")
